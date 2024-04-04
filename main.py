3f52a87bdbb78e93acadf8c899f880fae1492bba
MohamedElGazar1 <157542228+MohamedElGazar1@users.noreply.github.com>
2024-04-04 15:01:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,20 @@ from ultralytics import YOLO
 import io
 
 app = Flask(__name__)
-model = YOLO('FacialExpression.pt')
+
+# Initialize YOLO model
+try:
+    model = YOLO('FacialExpression.pt')
+except Exception as e:
+    print("Error initializing YOLO model:", e)
+    model = None
 
 def predict_yolo(image):
     try:
+        # Check if model is initialized
+        if model is None:
+            return "Error: YOLO model is not initialized."
+
         # Perform YOLO prediction
         results = model.predict(image)
         xx = results[0].to_json()
@@ -16,6 +26,7 @@ def predict_yolo(image):
 
         # Extract only the names
         class_names2 = [obj['name'] for obj in class_names_list]
+
         # Modify to handle output to count the objects
         word_counts = {}
         for word in class_names2:
@@ -35,15 +46,15 @@ def predict_yolo(image):
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image provided.'}), 400
+    try:
+        if 'image' not in request.files:
+            return jsonify({'error': 'No image provided.'}), 400
 
-    image = request.files['image']
+        image = request.files['image']
 
-    if image.filename == '':
-        return jsonify({'error': 'No image selected.'}), 400
+        if image.filename == '':
+            return jsonify({'error': 'No image selected.'}), 400
 
-    if image:
         img_data = image.read()
         img = Image.open(io.BytesIO(img_data))
 
@@ -51,6 +62,8 @@ def predict():
         prediction_result = predict_yolo(img)
 
         return jsonify({'result': prediction_result}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
