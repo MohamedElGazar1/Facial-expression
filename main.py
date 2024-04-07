7eb2585f6176b510b9be32a4ad4aef384bd23e69
MohamedElGazar1 <157542228+MohamedElGazar1@users.noreply.github.com>
2024-04-07 04:41:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,25 @@
 import json
+import cv2
 from PIL import Image
 from flask import Flask, request, jsonify
 from ultralytics import YOLO
 import io
 
-app = Flask(__name__)
 
-# Initialize YOLO model
-try:
-    model = YOLO('FacialExpression.pt')
-except Exception as e:
-    print("Error initializing YOLO model:", e)
-    model = None
+app = Flask(__name__)
+model = YOLO('FacialExpression.pt')
 
 def predict_yolo(image):
     try:
-        # Check if model is initialized
-        if model is None:
-            return "Error: YOLO model is not initialized."
-
         # Perform YOLO prediction
         results = model.predict(image)
-        xx = results[0].to_json()
+        # cv2.waitKey(0)
+        xx=results[0].tojson()
         class_names_list = json.loads(xx)
 
         # Extract only the names
         class_names2 = [obj['name'] for obj in class_names_list]
-
-        # Modify to handle output to count the objects
+        #modify to handle output to count the objects
         word_counts = {}
         for word in class_names2:
             if word in word_counts:
@@ -42,30 +34,26 @@ def predict_yolo(image):
         class_names_string = ' and '.join(output)
         return class_names_string
     except Exception as e:
-        return str(e)
+        return  str(e)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        if 'image' not in request.files:
-            return jsonify({'error': 'No image provided.'}), 400
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image provided.'}), 400
 
-        image = request.files['image']
+    image = request.files['image']
 
-        if image.filename == '':
-            return jsonify({'error': 'No image selected.'}), 400
+    if image.filename == '':
+        return jsonify({'error': 'No image selected.'}), 400
 
+    if image:
         img_data = image.read()
         img = Image.open(io.BytesIO(img_data))
 
         # Perform YOLO prediction
         prediction_result = predict_yolo(img)
 
-        return jsonify({'result': prediction_result}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify(prediction_result), 200
 
-# Run the Flask application with specified host and port
 if __name__ == '__main__':
-    # Use the following line to run Flask app on PythonAnywhere
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
